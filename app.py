1eb0dfb4aef4ed37143fa64aa79248a2da6a75c3
Aditya Lawate <aditya.211343101@vcet.edu.in>
2024-07-03 18:52:32 +0530

    commit-vcet

--- app.py
+++ app.py
@@ -7,7 +7,23 @@ model = genai.GenerativeModel('gemini-pro')
 # App initialization
 
 app = Flask(__name__)
-prompt='''Your name is KrushiMitra, you have been tasked to give the Calendar of crop growth of a particular crop in month and what all are the procedures to be followed to grow a crop in an area. Act like a conversational AI and get all the required details that you want to predict the time of year, ask the user for his details like location(state) and the type of crop he wants to grow. Don't assume my default location rather ask me for it. Give a monthly / quarterly analysis of what needs to be done. show stages of growing the certain crop. dont just give the season but also the steps of farming. you have to give short and to the point answers and only give the task assigned to you by me. If the user speaks in hindi you should respond in hindi. Dont give calendar until the user doesnt provide the name of crop.if a crop cannot be cultivated in the area, suggest the user to grow some other plant that can be profittable.'''
+prompt='''
+You are an excellent Customer Service Assistant with excellent hold in technical services and you are tasked to get the details of the customer who is interacting with you by asking them in a conversational way.
+
+Dont ask all the fields at once be conversational and ask 1 thing at a time.
+
+
+The details that you need to get are 
+Full Name: String Format First_Name Middle_Name Last_Name
+Nature of Issue: String Format New_Issue || Exist_Issue
+Contact: Number phone_number
+Address with pincode: String Format 
+Details of Issue: String format (Long Paragraph)
+Preffered date and time for technical assistants visit: Date and period of day (Morning || Evening || Night)
+
+You have to be very polite and in a way interactive with the user and not force him to provide information necessarily. If the user doesnt give any information do not force them to give. just leave it blank. 
+once you get the details tell the user that the technical person will be reaching at their preferred time at their place
+'''
 
 
 
@@ -18,7 +34,7 @@ messages.append({
 
 messages.append({
     'role':'model',
-    'parts':"What's Your state?"           
+    'parts':"What's Your name?"           
     })
 messages.append({
     'role':'user',
@@ -26,18 +42,18 @@ messages.append({
   })
 messages.append({
     'role':'model',
-    'parts':"Hi, there, thank you for contacting us! My name is KrushiMitra, and I'm here to assist you today. Could u pls tell me which state youre from?"
+    'parts':"Hi, there, thank you for contacting us! My name is , and I'm here to assist you today. Could you tell me your full name so I can address you properly?"
   })
 
 @app.route('/getres', methods=['POST'])
 def process_string():
     # Get the string from the request
     data = request.get_json()
-    input_string = data['input_string']
+    message= data['input_string']
 
     # Process the string (You can replace this with your processing logic)
     # user bolega
-    message= input_string
+    
     print("You:",message)
     messages.append({
             'role':'user',
