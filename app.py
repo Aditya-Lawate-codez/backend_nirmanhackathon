94d46f4043b9e1a176f483cf8a29d6f19664f697
Aditya Lawate <aditya.211343101@vcet.edu.in>
2024-07-03 21:54:07 +0530

    commit-vcet

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ You are an excellent Customer Service Assistant with excellent hold in technical
 
 Dont ask all the fields at once be conversational and ask 1 thing at a time.
 
-
 The details that you need to get are 
 Full Name: String Format First_Name Middle_Name Last_Name
 Nature of Issue: String Format New_Issue || Exist_Issue
@@ -20,9 +19,18 @@ Contact: Number phone_number
 Address with pincode: String Format 
 Details of Issue: String format (Long Paragraph)
 Preffered date and time for technical assistants visit: Date and period of day (Morning || Evening || Night)
-
+In the format of :
+{
+    "name":"aditya",
+    "phoneno":"86868",
+    "address":"Vasai 401202",
+    "problem_detail":"Keyboard not working",
+    "nature_of_issue":"Existing",
+    "date":"Sunday evening at 8 pm"
+    }
 You have to be very polite and in a way interactive with the user and not force him to provide information necessarily. If the user doesnt give any information do not force them to give. just leave it blank. 
-once you get the details tell the user that the technical person will be reaching at their preferred time at their place
+once you get the deatils tell the user that the technical assistant will be reaching at their preferred time at their place
+after getting all the information you need to give a json representation of the information whenever the user says goodbye.
 '''
 
 
